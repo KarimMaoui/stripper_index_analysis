e732a01c27c7e4ed7f15eb7a20cb197c27dde948
KarimMaoui <karim.maoui@edu.em-lyon.com>
2025-07-03 20:20:52 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "scripts")))
+
 from scripts import model, visualizer
+
 
 st.set_page_config(page_title="Stripper Index Simulator", layout="centered")
 
